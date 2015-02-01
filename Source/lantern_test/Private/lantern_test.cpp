bf1a7472c7f0c8a4b9bee1f5edb4ad20d79ba202
dnreyes <dnreyes>
2015-02-01 15:57:23 -0800

    Deleted Unneccesary Files
    Just cleaning up the folders, aka deleting lantern_test to make way for
    Uccallus

--- Source/lantern_test/Private/lantern_test.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
-
-#include "lantern_test.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, lantern_test, "lantern_test" );
-
-DEFINE_LOG_CATEGORY(Loglantern_test)
- 
