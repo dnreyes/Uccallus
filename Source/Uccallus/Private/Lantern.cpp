fa16beb6b0899be7ef1ddd2e612ad3fa09fe37dc
dnreyes <dnreyes>
2015-02-06 13:57:51 -0800

    Updated Lantern
    Lantern files WITH actual code inside (just set ups and simple inits.)

--- Source/Uccallus/Private/Lantern.cpp
+++ Source/Uccallus/Private/Lantern.cpp
@@ -1,8 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include <string>
+#include <vector>
 #include "Uccallus.h"
 #include "Lantern.h"
 
+using namespace std;
 
+ALantern::ALantern(const FObjectInitializer& ObjectInitalizer)
+	: Super(ObjectInitializer)
+{
+	capacity = 10;
+	cluCounter = 10;
+}
 
+void ALantern::pickUpClus() {
+	cluCounter++;
+}
 
+int ALantern::calculateMath(const vector<string> pieces) {
+
+}
+
+void ALantern::decreaseCounter() {
+	cluCounter--;
+}
+
+bool ALantern::lanternEmpty(){
+	return if (cluCounter == 0)
+}
+
+void ALantern::increaseCap() {
+	capacity++;
+}
+
+void OnPiecePickedUp(string piece) {
+
+}
--- Source/Uccallus/Public/Lantern.h
+++ Source/Uccallus/Public/Lantern.h
@@ -2,18 +2,42 @@
 
 #pragma once
 
+#include <string>
+#include <vector>
+
 #include "GameFramework/Actor.h"
 #include "Lantern.generated.h"
 
+using namespace std;
 /**
- * 
+ * Lantern Main Class. This class deals with the
+ * main functionality of the lantern class,
+ * such as checking if it is empty, what pieces
+ * it has, which function to use, etc.
  */
 UCLASS()
 class UCCALLUS_API ALantern : public AActor
 {
 	GENERATED_BODY()
+
+private:
+	void pickUpClus();
+
+	int calculateMath(const vector<string> pieces);
+
+	void decreaseCounter();
+
+	bool lanternEmpty();
+
+	void increaseCap();
+
+	void OnPiecePickedUp(string piece);
+
+public:
+	string pieceName;
 	
+	int cluCounter;
 	
-	
-	
+	int capacity;
+
 };
